881f3d0774b9b82173d6f357bb11167ed40a6747
kamran choudhry <113056580+kamranchoudhry@users.noreply.github.com>
2025-03-13 02:52:18 -0400

    added final score and timer

--- Assets/Scripts/EndGame.cs
+++ Assets/Scripts/EndGame.cs
@@ -2,10 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
+
 
 public class EndGame : MonoBehaviour
 {
+
+  public TMP_Text FinalTime;
+  public TMP_Text FinalScore;
+
     // Start is called before the first frame update
+     void Start()  
+    {
+        if (GameManager.Instance != null) 
+        {
+            FinalScore.text = "Score: " + GameManager.Instance.score;
+            FinalTime.text = "Time: " + Mathf.FloorToInt(GameManager.Instance.timer).ToString();
+        }
+        else
+        {
+            Debug.LogError("GameManager instance not found!");
+        }
+    }
     public void RePlay(){
     SceneManager.LoadScene("SampleScene");
     GameManager.Instance.score = 0;
